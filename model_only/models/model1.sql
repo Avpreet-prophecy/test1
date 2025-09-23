13f5c9e4866f9bd8e8822a3e37cc04db4f940bee
avpreet@prophecy.io <avpreet@prophecy.io>
2025-09-23 08:12:33 +0000

    test1

---
+++ model_only/models/model1.sql
@@ -0,0 +1,38 @@
+WITH customers AS (
+
+  SELECT * 
+  
+  FROM {{ source('qa_team.avpreettables', 'customers') }}
+
+),
+
+s1 AS (
+
+  SELECT * 
+  
+  FROM {{ ref('s1')}}
+
+),
+
+duplicate_finder AS (
+
+  {{
+    DatabricksSqlBasics.FindDuplicates(
+      's1', 
+      [], 
+      '', 
+      'unique', 
+      '', 
+      '', 
+      '', 
+      'allCols', 
+      ['a', 'b', 'c'], 
+      []
+    )
+  }}
+
+)
+
+SELECT *
+
+FROM customers
