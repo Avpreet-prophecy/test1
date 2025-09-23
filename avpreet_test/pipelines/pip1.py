825910dc3bc644745d5fdb1c55273465f2bbfc6f
avpreet@prophecy.io <avpreet@prophecy.io>
2025-09-23 09:13:14 +0000

    Replace seed 's1' with 'dummy' and remove 'model1' in 'pip1' pipeline.

--- avpreet_test/pipelines/pip1.py
+++ avpreet_test/pipelines/pip1.py
@@ -2,11 +2,11 @@ Schedule = Schedule(cron = "* 0 2 * * * *", timezone = "GMT", emails = ["email@g
 SensorSchedule = SensorSchedule(enabled = False)
 
 with DAG(Schedule = Schedule, SensorSchedule = SensorSchedule):
-    numeric_values_unknown_format = Task(
-        task_id = "numeric_values_unknown_format", 
+    dummy = Task(
+        task_id = "dummy", 
         component = "Dataset", 
-        table = {"name" : "s1", "sourceType" : "Seed"}, 
-        writeOptions = {"writeMode" : "overwrite"}
+        writeOptions = {"writeMode" : "overwrite"}, 
+        table = {"name" : "dummy", "sourceType" : "Seed", "alias" : ""}
     )
     pip1__find_duplicates_in_m1 = Task(
         task_id = "pip1__find_duplicates_in_m1", 
@@ -19,4 +19,4 @@ with DAG(Schedule = Schedule, SensorSchedule = SensorSchedule):
         modelName = "pip1__parse_person_details"
     )
     model1_1 = Task(task_id = "model1_1", component = "Model", modelName = "model1")
-    numeric_values_unknown_format.out >> pip1__find_duplicates_in_m1.in_0
+    dummy.out >> pip1__find_duplicates_in_m1.in_0
