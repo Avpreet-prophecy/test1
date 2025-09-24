2b39129f4c481f0e3e141bda8f7e09c70928dc07
avpreet@prophecy.io <avpreet@prophecy.io>
2025-09-24 02:25:49 +0000

    Add `support_tickets`, `uber_data`, and `rides` datasets to `pip2` pipeline and update source schema

--- ext_pro/pipelines/pip2.py
+++ ext_pro/pipelines/pip2.py
@@ -4,3 +4,24 @@ with DAG():
         component = "Model", 
         modelName = "pip2__category_avg_salary"
     )
+    support_tickets = Task(
+        task_id = "support_tickets", 
+        component = "Dataset", 
+        writeOptions = {"writeMode" : "overwrite"}, 
+        table = {"name" : "support_tickets", "sourceName" : "avpreet_random_prophecy_io_team.dummy", "sourceType" : "Table"}
+    )
+    uber_data = Task(
+        task_id = "uber_data", 
+        component = "Dataset", 
+        table = {
+          "name": "uber_data", 
+          "sourceType": "Source", 
+          "sourceName": "avpreet_random_prophecy_io_team.dummy", 
+          "alias": ""
+        }
+    )
+    rides = Task(
+        task_id = "rides", 
+        component = "Dataset", 
+        table = {"name" : "rides", "sourceType" : "Source", "sourceName" : "avpreet_random_prophecy_io_team.dummy", "alias" : ""}
+    )
