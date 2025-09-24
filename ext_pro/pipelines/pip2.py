a87561d81266339b95283caca04e2b7b07db376c
avpreet@prophecy.io <avpreet@prophecy.io>
2025-09-24 02:23:30 +0000

    added some pipeline parameters only

--- ext_pro/pipelines/pip2.py
+++ ext_pro/pipelines/pip2.py
@@ -1,4 +1,6 @@
-with DAG():
+Config = {"c_string" : "'hey'", "c_int" : "'123'"}
+
+with DAG(Config = Config):
     pip2__age_21_and_above_employees = Task(
         task_id = "pip2__age_21_and_above_employees", 
         component = "Model", 
