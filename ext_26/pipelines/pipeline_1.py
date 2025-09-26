73a03b9f86d7890854b5e787acb210a0a30b6ae5
avpreet@prophecy.io <avpreet@prophecy.io>
2025-09-26 10:36:25 +0000

    Add dataset avpreet_random_prophecy_io_team.dummy.rides

--- ext_26/pipelines/pipeline_1.py
+++ ext_26/pipelines/pipeline_1.py
@@ -2,4 +2,8 @@ Schedule = Schedule(cron = "* 0 2 * * * *", timezone = "GMT", emails = ["email@g
 SensorSchedule = SensorSchedule(enabled = False)
 
 with DAG(Schedule = Schedule, SensorSchedule = SensorSchedule):
-    pass
+    rides = Task(
+        task_id = "rides", 
+        component = "Dataset", 
+        table = {"name" : "rides", "sourceType" : "Table", "sourceName" : "avpreet_random_prophecy_io_team.dummy", "alias" : ""}
+    )
