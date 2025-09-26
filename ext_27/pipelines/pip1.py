dd9cc5bd96a6863b648563346f807acc9a5cd644
avpreet@prophecy.io <avpreet@prophecy.io>
2025-09-26 10:44:13 +0000

    AI: filter rides dataset to exclude rows where fare_amount is null

--- ext_27/pipelines/pip1.py
+++ ext_27/pipelines/pip1.py
@@ -1,9 +1,6 @@
-Schedule = Schedule(cron = "* 0 2 * * * *", timezone = "GMT", emails = ["email@gmail.com"], enabled = False)
-SensorSchedule = SensorSchedule(enabled = False)
-
-with DAG(Schedule = Schedule, SensorSchedule = SensorSchedule):
-    rides = Task(
-        task_id = "rides", 
-        component = "Dataset", 
-        table = {"name" : "rides", "sourceType" : "Table", "sourceName" : "avpreet_random_prophecy_io_team.dummy", "alias" : ""}
+with DAG():
+    pip1__rides_with_fare = Task(
+        task_id = "pip1__rides_with_fare", 
+        component = "Model", 
+        modelName = "pip1__rides_with_fare"
     )
