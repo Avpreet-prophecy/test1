5cee50e9689c096f060cc185323a1d6c8c1d374e
avpreet@prophecy.io <avpreet@prophecy.io>
2025-09-26 11:15:39 +0000

    Add dataset avpreet_random_prophecy_io_team.dummy.uber_data

--- ext_26/pipelines/pipeline_1.py
+++ ext_26/pipelines/pipeline_1.py
@@ -1,4 +1,9 @@
 with DAG():
+    uber_data = Task(
+        task_id = "uber_data", 
+        component = "Dataset", 
+        table = {"name" : "uber_data", "sourceType" : "Table", "sourceName" : "avpreet_random_prophecy_io_team.dummy", "alias" : ""}
+    )
     pipeline_1__customer_spend_rank = Task(
         task_id = "pipeline_1__customer_spend_rank", 
         component = "Model", 
